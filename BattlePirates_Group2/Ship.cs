413319b4d2145c3f564708dd024757674b07ecee
Peter Chan <chan-p@outlook.com>
2015-03-01 20:40:10 -0800

    deleted button

---
+++ BattlePirates_Group2/Ship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattlePirates_Group2
+{
+    class Ship
+    {
+        private int health;
+        private bool sunk;
+        private bool activated;
+        private int[][] location;
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value;
+            }
+        }
+
+        public bool Sunk
+        {
+
+        }
+    }
+}
