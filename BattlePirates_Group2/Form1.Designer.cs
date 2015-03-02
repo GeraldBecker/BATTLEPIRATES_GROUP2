f87931890eaec874b7582d2874b7c21883bda57b
Gerald Becker <ger_0_1@hotmail.com>
2015-03-01 16:13:01 -0800

    The blue wilson button.

--- BattlePirates_Group2/Form1.Designer.cs
+++ BattlePirates_Group2/Form1.Designer.cs
@@ -24,6 +24,7 @@
         /// </summary>
         private void InitializeComponent() {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -36,11 +37,24 @@
             this.label1.Text = "POO";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aqua;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(105, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wilson Button";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -52,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
