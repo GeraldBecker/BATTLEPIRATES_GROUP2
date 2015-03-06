22b78f1f4bcdfb7304445f5933d455691db6bc2f
Gerald Becker <ger_0_1@hotmail.com>
2015-03-05 21:23:18 -0800

    Fixed the Ship placement code. Cleaned up unneeded code.

--- BattlePirates_Group2/shipPlacementForm.cs
+++ BattlePirates_Group2/shipPlacementForm.cs
@@ -10,19 +10,20 @@ using System.Windows.Forms;
 
 namespace BattlePirates_Group2 {
     public partial class shipPlacementForm : Form {
-
+        private const int BLOCKWIDTH = 25;
         private bool dragging;
 
-
+        
         private BaseShip[] myShips = new BaseShip[6];
 
 
+        private bool containsTrue;
+        private Point[] tempShapePoints;
+        private bool tempOrientVert;
+        private int shipToMove;
+
         public shipPlacementForm() {
             InitializeComponent();
-
-            /*ship1 = new ManowarShip();
-            ship1.setLocation(new Point[] { new Point(5, 15), new Point(6, 15), new Point(7, 15), new Point(8, 15), new Point(9, 15) }, true);*/
-
 
             //5 square size
             myShips[0] = new ManowarShip();
@@ -48,51 +49,46 @@ namespace BattlePirates_Group2 {
             myShips[5].setLocation(new Point[] { new Point(5, 23), new Point(6, 23) }, true);
 
             dragging = false;
+
+            //Prevent the start game button from taking focus
+            startGameButton.TabStop = false;
         }
 
         private void shipPlacementForm_Paint(object sender, PaintEventArgs e) {
             for(int r = 0; r < 10; r++) {
                 for(int c = 0; c < 10; c++) {
-                    e.Graphics.FillRectangle(new SolidBrush(Color.Aqua), r * 25, c * 25, 20, 20);
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Aqua), r * BLOCKWIDTH, c * BLOCKWIDTH, BLOCKWIDTH - 5, BLOCKWIDTH - 5);
                     
                 }
             }
 
-            Point[] points;// = ship1.getLocation();
-            /*for(int i = 0; i < points.Length; i++) {
-                e.Graphics.FillRectangle(new SolidBrush(Color.Orange), points[i].Y * 25, points[i].X * 25, 20, 20);
-            }*/
+            Point[] points;
 
 
             //array of ships
             for(int n = 0; n < myShips.Length; n++) {
                 points = myShips[n].getLocation();
                 for(int i = 0; i < points.Length; i++) {
-                    e.Graphics.FillRectangle(new SolidBrush(Color.Orange), points[i].Y * 25, points[i].X * 25, 20, 20);
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Orange), points[i].Y * BLOCKWIDTH, points[i].X * BLOCKWIDTH, BLOCKWIDTH - 5, BLOCKWIDTH - 5);
                 }
             }
 
 
             if(tempShapePoints != null && dragging) {
                 for(int i = 0; i < tempShapePoints.Length; i++) {
-                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), tempShapePoints[i].Y * 25, tempShapePoints[i].X * 25, 20, 20);
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), tempShapePoints[i].Y * BLOCKWIDTH, tempShapePoints[i].X * BLOCKWIDTH, BLOCKWIDTH - 5, BLOCKWIDTH - 5);
                 }
             }
         }
 
 
-        int origRow;
-        int origCol;
-        bool containsTrue;
-        Point[] tempShapePoints;
-        bool tempOrientVert;
-        int shipToMove;
+        
 
         private void shipPlacementForm_MouseDown(object sender, MouseEventArgs e) {
             
             Console.WriteLine("Mouse Down Location: " + e.X + " " + e.Y);
-            int c = e.X / 25;
-            int r = e.Y / 25;
+            int c = e.X / BLOCKWIDTH;
+            int r = e.Y / BLOCKWIDTH;
             Console.WriteLine("Point: " + r + " " + c);
 
 
@@ -101,8 +97,6 @@ namespace BattlePirates_Group2 {
                 if(myShips[n].containsPoint(new Point(r, c))) {
                     Console.WriteLine("YOU HIT A SHIP");
 
-                    origRow = r;
-                    origCol = c;
                     containsTrue = true;
 
                     tempShapePoints = myShips[n].getLocation().ToArray<Point>();
@@ -112,66 +106,26 @@ namespace BattlePirates_Group2 {
                 }
             }
 
-
-                
-            
-
-
-
-
-
-            /*if(ship1.containsPoint(new Point(r, c))) {
-                Console.WriteLine("YOU HIT A SHIP");
-
-                origRow = r;
-                origCol = c;
-                containsTrue = true;
-
-                tempShapePoints = ship1.getLocation().ToArray<Point>();
-                tempOrientVert = ship1.isVertical();
-            }*/
             dragging = true;
         }
 
         private void shipPlacementForm_MouseMove(object sender, MouseEventArgs e) {
             if(dragging && containsTrue) {
-                Console.WriteLine("Mouse UP Location: " + e.Y / 25 + " " + e.X / 25);
-                int moveR = origRow - e.Y/25;
-                int moveC = origCol - e.X/25;
+                Console.WriteLine("Mouse UP Location: " + e.Y / BLOCKWIDTH + " " + e.X / BLOCKWIDTH);
+                
 
 
                 if(tempOrientVert) {
                     for(int i = 0; i < tempShapePoints.Length; i++) {
-                        tempShapePoints[i].X = (e.Y + (i * 25)) / 25;
-                        tempShapePoints[i].Y = (e.X) / 25;
+                        tempShapePoints[i].X = (e.Y + (i * BLOCKWIDTH)) / BLOCKWIDTH;
+                        tempShapePoints[i].Y = (e.X) / BLOCKWIDTH;
                     }
 
-                    /*
-                    tempShapePoints[0].X = e.Y / 25;
-                    tempShapePoints[0].Y = e.X / 25;
-                    tempShapePoints[1].X = (e.Y + (1 * 25)) / 25;
-                    tempShapePoints[1].Y = (e.X) / 25;
-                    tempShapePoints[2].X = (e.Y + (2 * 25)) / 25;
-                    tempShapePoints[2].Y = (e.X) / 25;
-                    tempShapePoints[3].X = (e.Y + (3 * 25)) / 25;
-                    tempShapePoints[3].Y = (e.X) / 25;
-                    tempShapePoints[4].X = (e.Y + (4 * 25)) / 25;
-                    tempShapePoints[4].Y = (e.X) / 25;*/
                 } else {
                     for(int i = 0; i < tempShapePoints.Length; i++) {
-                        tempShapePoints[i].X = (e.Y) / 25;
-                        tempShapePoints[i].Y = (e.X + (i * 25)) / 25;
+                        tempShapePoints[i].X = (e.Y) / BLOCKWIDTH;
+                        tempShapePoints[i].Y = (e.X + (i * BLOCKWIDTH)) / BLOCKWIDTH;
                     }
-                    /*tempShapePoints[0].X = e.Y / 25;
-                    tempShapePoints[0].Y = e.X / 25;
-                    tempShapePoints[1].X = e.Y / 25;
-                    tempShapePoints[1].Y = (e.X + (1 * 25)) / 25;
-                    tempShapePoints[2].X = (e.Y) / 25;
-                    tempShapePoints[2].Y = (e.X + (2 * 25)) / 25;
-                    tempShapePoints[3].X = (e.Y) / 25;
-                    tempShapePoints[3].Y = (e.X + (3 * 25)) / 25;
-                    tempShapePoints[4].X = (e.Y) / 25;
-                    tempShapePoints[4].Y = (e.X + (4 * 25)) / 25;*/
                 }
                 
 
@@ -184,12 +138,6 @@ namespace BattlePirates_Group2 {
         private void shipPlacementForm_MouseUp(object sender, MouseEventArgs e) {
             dragging = false;
             if(containsTrue) {
-                /*Point[] orig = ship1.getLocation();
-                int moveR = origRow - e.Y / 25;
-                int moveC = origCol - e.X / 25;
-                //temp.setLocation(new Point[] { new Point(orig[0].X - moveR, orig[0].Y - moveC), new Point(1, 3), new Point(1, 4), new Point(1, 5), new Point(1, 6) });
-                ship1.setLocation(tempShapePoints, false);*/
-
                 myShips[shipToMove].setLocation(tempShapePoints, tempOrientVert);
             }
             
@@ -209,20 +157,10 @@ namespace BattlePirates_Group2 {
                     for(int i = 0; i < tempShapePoints.Length; i++) {
                         tempShapePoints[i].X = tempShapePoints[0].X;
                     }
-
-                    /*tempShapePoints[1].X = tempShapePoints[0].X;
-                    tempShapePoints[2].X = tempShapePoints[0].X;
-                    tempShapePoints[3].X = tempShapePoints[0].X;
-                    tempShapePoints[4].X = tempShapePoints[0].X;*/
                 } else {
                     for(int i = 0; i < tempShapePoints.Length; i++) {
                         tempShapePoints[i].Y = tempShapePoints[0].Y;
                     }
-                    /*tempShapePoints[0].Y = tempShapePoints[0].Y;
-                    tempShapePoints[1].Y = tempShapePoints[0].Y;
-                    tempShapePoints[2].Y = tempShapePoints[0].Y;
-                    tempShapePoints[3].Y = tempShapePoints[0].Y;
-                    tempShapePoints[4].Y = tempShapePoints[0].Y;*/
                 }
                 
                 
@@ -237,7 +175,7 @@ namespace BattlePirates_Group2 {
         }
 
         private void startGame_click(object sender, EventArgs e) {
-
+            
         }
 
         
