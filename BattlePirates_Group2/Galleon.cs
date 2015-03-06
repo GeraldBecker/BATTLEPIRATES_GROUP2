406790ba566c233e8c55f456bf4de523d3a5cf6e
WilsonCarpenter <wilsonkcarpenter@gmail.com>
2015-03-05 20:07:49 -0800

    recommit

---
+++ BattlePirates_Group2/Galleon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattlePirates_Group2 {
+    class Galleon : BaseShip {
+        const int SHIPSIZE = 4;
+
+        public Galleon() : base(SHIPSIZE) {
+            
+        }
+    }
+}
